11157d4dd532a248cb83b5482af05e3b1f95c6e0
LENOVO <risasetiawati12@gmail.com>
2024-01-27 16:43:00 +0700

    first init github

---
+++ page/tambah_nota.php
@@ -0,0 +1,45 @@
+<?php 
+
+extract($_POST);
+
+$id_nota = date('YmdHis');
+$tgl_keluar = date('Y-m-d H:i:s');
+$q_insert_nota = mysqli_query($koneksi, "INSERT INTO nota (id_nota, tgl_keluar, nama_pembeli, total, bayar, kembalian, username) 
+    VALUES('". $id_nota . "','" . $tgl_keluar . "','" . $nama_pembeli . "','" . $total . "','" . $bayar . "','" . $kembali . "','admin'); ");
+
+$total_row = count($id_barang);
+
+for ($i = 0; $i < $total_row; $i++) {
+    $input_id = $id_barang[$i];
+    $input_qty = $qty[$i];
+    $input_harga = $harga[$i];
+
+    if (($input_id != '') && ($input_qty != '')) {
+        $total_harga = $input_qty * $input_harga;
+        $q_insert_pengeluaran = mysqli_query($koneksi, "INSERT INTO pengeluaran(id_nota,id_barang,harga_jual,jml_keluar,total_harga) 
+        VALUES('". $id_nota . "','". $input_id . "','" . $input_harga . "','" . $input_qty . "','" . $total_harga . "');");
+        
+    }
+}
+
+
+if($q_insert_nota){
+    $output = 'Berhasil Menyimpan Data Nota ';
+}else{
+    $output ='Gagal Menyimpan Data Nota ';
+}
+
+
+// echo json_encode($output);
+
+?>
+
+<h1><?= $output ?></h1>
+
+<hr />
+<a href="http://localhost/Plastik/" class="btn btn-success">KEMBALI KE HOME</a>
+<a href="http://localhost/Plastik/print_nota.php?id_nota=<?= $id_nota ?>" target="_blank" class="btn btn-primary">PRINT
+    NOTA (Kecil)</a>
+<a href="http://localhost/Plastik/print_faktur.php?id_nota=<?= $id_nota ?>" target="_blank"
+    class="btn btn-primary">PRINT
+    NOTA (Besar)</a>
